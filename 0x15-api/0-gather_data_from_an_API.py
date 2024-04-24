2e29e5b97af5ada02220054ed6fd064b3241c14e
nzaumuimi <nzaumuimi@gmail.com>
2024-04-24 11:41:47 -0700

    0x15-api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -28,4 +28,4 @@ if __name__ == '__main__':
           .format(employeeName, done, len(tasks)))
 
     for task in done_tasks:
-        print("\t {}".format(task.get('title'))
+        print("\t {}".format(task.get('title')))
